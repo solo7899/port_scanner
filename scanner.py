221cf856f51c21968f905d89c934caeecd065672
hamidreza <dolati.hamidreza78@gmail.com>
2025-07-20 16:15:37 +0330

    port validation added

--- scanner.py
+++ scanner.py
@@ -13,16 +13,65 @@ def parse_args():
     return parser.parse_args()
 
 class Scanner():
+
+    errs = {
+        "range_value_err": "*** Error: please Enter the correct format .e.g 1-1000(with on spaces in between)\n*** values all should be numbers",
+        "single_value_err": "*** Error: values should all be positive numeric values from 1-65535"
+        }
+
     def __init__(self, target, ports, banner, verbose):
         self.target = target
         self.ports = ports
         self.banner = banner
         self.verbose = verbose
     
+    
+    def __check_range(self, *args):
+        for arg in args:
+            if not (1 <= arg <= 65535) :
+                return False
+        return True
+    
     def parse_ports(self):
-        pass
+        port_exprs = self.ports.split(',')
+        self.ports = []
+        for p in port_exprs:
+            if '-' in p:
+                port_start, port_end = p.split('-')
+                try:
+                    port_start, port_end = int(port_start), int(port_end)
+                except ValueError:
+                    print(self.errs['range_value_err'])
+                    return False
+
+                if not self.__check_range(int(port_start), int(port_end)):
+                    print(self.errs["single_value_err"])
+                    return False
+                    
+                for p in range(int(port_start), int(port_end)+1):
+                    self.ports.append(p)
+                
+            else:
+                try:
+                    p = int(p)
+                except ValueError:
+                    print(self.errs["single_value_err"]) 
+                    return False
+
+                if not self.__check_range(int(p)):
+                    print(self.errs["single_value_err"])
+                    return False
+                self.ports.append(p)
+                
+            
+        self.ports = sorted(set(self.ports))
+        return True
 
 if __name__ == '__main__':
     args = parse_args()
 
     scanner = Scanner(args.target, args.ports, args.banner, args.verbose)
+    if not scanner.parse_ports():
+        exit(1)
+
+print(scanner.ports)
